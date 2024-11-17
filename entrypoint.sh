553b0fb6c925fe658fd6aab78fd1ff250f5498ab
Alex386386 <mamonov40600@mail.ru>
2024-11-17 14:35:55 -0300

    celery security, example of workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+create_user() {
+  if id "celeryuser" &>/dev/null; then
+    echo "Пользователь celeryuser уже существует, пропускаем создание."
+  else
+    echo "Создание пользователя celeryuser..."
+    adduser --disabled-password --gecos '' celeryuser
+  fi
+}
+
 if [ "$CONTAINER_TYPE" == "backend" ]; then
   echo "Waiting for postgres..."
   sleep 5
@@ -7,5 +16,8 @@ if [ "$CONTAINER_TYPE" == "backend" ]; then
   uvicorn src.main:app --host 0.0.0.0 --port 8000
 elif [ "$CONTAINER_TYPE" == "celery" ]; then
   echo "Запуск Celery worker..."
-  celery -A src.celery_tasks.celery worker --loglevel=info
+
+  create_user
+
+  su celeryuser -c 'celery -A src.celery_tasks.celery worker --loglevel=INFO'
 fi
